43fff622547c58d27d82965b51a17545775e0dca
fathsam82 <fathsam82@gmail.com>
2023-08-04 17:59:46 -0600

    gradle dependency additions

---
+++ MiloSchedule/src/main/java/com/skilldistillery/miloschedule/MiloScheduleApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.miloschedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiloScheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiloScheduleApplication.class, args);
+	}
+
+}
---
+++ MiloSchedule/src/test/java/com/skilldistillery/miloschedule/MiloScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.miloschedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiloScheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
